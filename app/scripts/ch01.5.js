e7d3cc612c4da9cd8a18660fdbf00bfde5326b3e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-23 06:17:02 +0200

    Chapter 1: Demo 1.5, Primitive (a.k.a Simple) Values

---
+++ app/scripts/ch01.5.js
@@ -0,0 +1,31 @@
+	'use strict';
+
+	console.log('Chapter 1: Demo 1.5, Primitive (a.k.a Simple) Values');
+
+	var myString = 'string';
+	var myNumber = 10;
+	var myBoolean = false;
+	var myNull = null;
+	var myUndefind = myUndefind;
+
+	console.log(myString, myNumber, myBoolean, myNull, myUndefind);
+
+
+	/*
+	Consider that a complex object like array or object can be made up of multiple primitive values, and thus becomes a complex set of  multiple values
+	*/
+
+	var myObject = {
+		myString: 'string',
+		myNumber: 10,
+		myBoolean: true,
+		myNull: null,
+		myUndefind: myUndefind
+	};
+
+	console.log(myObject);
+
+
+	var myArray = ['string', 10, false, null, undefined];
+
+	console.log(myArray);
