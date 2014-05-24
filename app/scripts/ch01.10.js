f95bc4f0d117e826f52da6515da06f5d58083a4e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-24 07:20:55 +0200

    Chapter 1: Demo 1.10, The String, Number, and Boolean Primitive Values Act Like Objects When Used Like Objects

---
+++ app/scripts/ch01.10.js
@@ -0,0 +1,29 @@
+	'use strict';
+
+	console.log('Chapter 1: Demo 1.9, The String, Number, and Boolean Primitive Values Act Like Objects When Used Like Objects');
+
+	console.log('The native Object Constructors "Object(), Array(), Function(), Date(), Error() and RegExp()" are complex becasue they can contain one or more primitive or complex values.');
+
+	var object = {
+		myString: 'string hello there',
+		myNumber: 10,
+		myBoolean: false,
+		myNull: null,
+		myUndefined: undefined
+	};
+
+	console.log(object);
+
+	var array = ['string hello there', 10, false, null, undefined];
+
+	console.log(array);
+
+	/*
+	Contrast this to the simplicity of the primitive values below. In Primitive from, noen of the values below can be more complex than what you see while complex values can encapsulate any of the JavaScript values (seen above)
+	*/
+
+	var myString = 'string hello there 2';
+	var myNumber = 10;
+	var myBoolean = false;
+	var myNull = null;
+	var myUndefined = undefined;
