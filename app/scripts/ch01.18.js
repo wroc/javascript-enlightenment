7ed5b7a6e7dc28d07d4538eeaaf9beff13467a66
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-27 05:28:45 +0200

    Chapter 1: Demo 1.17.1, An Instance Create From a Constructor Can Have Its Own Independent Properties (Instance Properties)

---
+++ app/scripts/ch01.18.js
@@ -0,0 +1,10 @@
+	'use strict';
+
+	console.log('Chapter 1: Demo 1.17, An Instance Create From a Constructor Can Have Its Own Independent Properties (Instance Properties)');
+
+	console.log('In JavaScript Objects can be augmented at any time. Mutable objects');
+
+	var myArray= new Array();
+	myArray.prop = 'test';
+
+	console.log(myArray.prop);
