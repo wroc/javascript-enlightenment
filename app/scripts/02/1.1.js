e4c787751f6ca461f4e3ea96e26972f048178071
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-27 06:12:05 +0200

    Chapter 2

---
+++ app/scripts/02/1.1.js
@@ -0,0 +1,7 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 1.1, Complex Object Can Contain Most of the JavaScript Values as Properties');
+
+	console.log('A Complex object can have any permitted JavaScript value. Below i create an Object  object called myObject and then properties representing the majority of values available in JavaScript');
+
+	
---
+++ app/scripts/ch01.19.js
@@ -0,0 +1,8 @@
+	'use strict';
+
+	console.log('Chapter 1: Demo 1.19, The Semantics of ""JavaScript objects" and "Object()" Objects');
+
+	console.log('Do not confuse the general term "JavaScript Objects", which referes to the notion of objects in JavaScript, with Object(); objects. ');
+
+	var myObject = new Object(); // Object(); is its own very specific kind of value
+	var myObject = 'myValue'; //this whole line is generally an object
