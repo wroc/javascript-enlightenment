15142805133c3a5cd2c9989dfcb749183e9533e7
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-29 06:41:51 +0200

    Chapter 2: Demo 5.1, How Referance to Object Properties Are Resolved

---
+++ app/scripts/02/5.1.js
@@ -0,0 +1,9 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 4.1, How Referance to Object Properties Are Resolved');
+
+	console.log('JavaScript will look in two places, Array.prototype and then Object.prototype');
+
+	var myArray = [];
+
+	console.log(myArray.foo);
