eed124b4f64bd77aa6a41e6399fa0aad147d8a3c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-29 06:11:01 +0200

    Chapter 2: Demo 3.1, Getting/Setting/Updating an Objects Properties Using Dot Notation or Bracket Notation');

---
+++ app/scripts/02/3.4.js
@@ -0,0 +1,13 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 3.1, Getting/Setting/Updating an Objects Properties Using Dot Notation or Bracket Notation');
+
+	console.log('Additionally, bracket Notation can come in handy fo rgetting a property names that are invalid JavaScript identifires. Below i use a number and a reserved keyword as a property name(valid) as a string) that only bracket notation can access');
+
+	var myObject = { '123': 'zero', 'class': 'foobar' };
+
+	// lets see dot notation do this! keep in mind 'class' is a JavaScript keyword
+	console.log(myObject['123'], myObject['class']);
+
+	// it can't do what bracket notation can do, in fact it causes an error
+	// console.log(myObject.0, myObject.class);
