f3294351fe9153f3e3db26710d8ce99fc3bbf5cf
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-29 06:01:30 +0200

    hapter 2: Demo 3.1, Getting/Setting/Updating an Objects Properties Using Dot Notation or Bracket Notation

---
+++ app/scripts/02/3.3.js
@@ -0,0 +1,12 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 3.1, Getting/Setting/Updating an Objects Properties Using Dot Notation or Bracket Notation');
+
+	console.log('Advantage of bracket notation over dot notation by using it to access the property foobar. doing this with  tw variables that , when joine, produce the string version of the property key contained in the foobarObject');
+
+	var foobarObject = {foobar: 'Foobar is code for no code' };
+
+	var string1 = 'foo';
+	var string2 = 'bar';
+
+	console.log('foobarObject[string1 + string2]');
