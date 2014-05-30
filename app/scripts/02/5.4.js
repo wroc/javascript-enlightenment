819e3ae208482f24191b9cbf3c209aca1cd9a402
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-30 07:03:53 +0200

    Chapter 2: Demo 5.2, How References to Object Properties Are Resolved | to locale string end

---
+++ app/scripts/02/5.4.js
@@ -0,0 +1,7 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 5.4, How References to Object Properties Are Resolved | to local string end');
+
+	var myArray = ['foo', 'bar'];
+
+	console.log(myArray.toLocaleString()); 
