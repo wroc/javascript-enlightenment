32b278991504d258e921b5ea3949fcff2791f66b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-05-30 06:53:57 +0200

    Chapter 2: Demo 5.2, How References to Object Properties Are Resolved | Has own Propert join

---
+++ app/scripts/02/5.3.js
@@ -0,0 +1,7 @@
+	'use strict';
+
+	console.log('Chapter 2: Demo 5.3, How References to Object Properties Are Resolved | has own property join');
+
+	var myArray = ['foo', 'bar'];
+
+	console.log(myArray.hasOwnProperty('join')); 
