469a5b4a037e5033bb54e6f82f135415c8235f76
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-01 21:30:32 +0200

    Chapter 2: Demo 5.8, Host Objects versus Native Objects

--- app/scripts/02/5.8.js
+++ app/scripts/02/5.8.js
@@ -1,6 +1,6 @@
-	'use strict';
 
-	console.log('Chapter 2: Demo 5.8, Host Objects versus Native Objects ');
+
+	console.log('Chapter 2: Demo 5.8, Host Objects versus Native Objects');
 
 	for (x in window.document) {
 		console.log();
