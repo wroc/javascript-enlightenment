0aa0e95a735ecdd02f480c0aa390242def6fcba4
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-02 06:35:03 +0200

    Chapter 3: Demo 2.1, Object Parameters

--- app/scripts/03/2.1.js
+++ app/scripts/03/2.1.js
@@ -1,11 +1,17 @@
 
 
 	console.log('Chapter 3: Demo 2.1, Object Parameters');
-	console.log('Object() constructor function takes one optional perameter. That perameter is the value you woudl like to create, if you provide no perameter, then null or undefined value will be assumed');
+	console.log('If a value besides null or undefined i passed to the object() constructor, the value passsed wil be created as an object. so theoretially we can us the Object() constructor to create any of teh other native object that have a constructor ');
 
-	//create an empty  object with no properties
-	var manny = new Object();
-	var manny2 = new Object(undefined);
-	var manny3 = new Object(null);
+	//* Use Object() constructor to create a string, number, array, function, boolean and regex object
+	// logs below confirm object creation
 
-	console.log(typeof manny, typeof manny2, typeof manny3);
+	console.log(new Object('foo'));
+	console.log(new Object(1));
+	console.log(new Object([]));
+	console.log(new Object(function(){}));
+	console.log(new Object(true));
+	console.log(new Object(/\bt[a-z]+\b/));
+
+	console.log();
+	console.log('creating a string, number, array, function, boolean, and regex object instance via the Object() constructor is never done. this is for demonstration purpouses');
