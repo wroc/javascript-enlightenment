2bfe3e39c9a7c91320d40b42726f7b2e15fd307d
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-03 05:15:37 +0200

    Chapter 3: Demo 3.2, Creating Object() Objects Using 'Object Literals using as specified as strings

---
+++ app/scripts/03/3.3.js
@@ -0,0 +1,14 @@
+	'use strict';
+
+	console.log('Chapter 3: Demo 3.2, Creating Object() Objects Using "Object Literals"');
+
+	console.log('Using as strings: Creating an object literal specifired as strings');
+
+	var manny = {
+		'living': true,
+		'age': 31,
+		'gender': 'male',
+		'getGender': function() { return manny.gender; }
+	};
+
+	console.log(manny);
