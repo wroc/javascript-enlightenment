625fb1fab07182a8ec1c84423ec51855b5cda2a2
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-15 23:10:36 +0200

    Chapter 5: Demo 2.1, Global Functions Contained Within the Head Object

--- app/scripts/05/2.1.js
+++ app/scripts/05/2.1.js
@@ -1,3 +1,16 @@
 	'use strict';
 
 	console.log('Chapter 5: Demo 1.1, Global Functions Contained Within the Head Object');
+
+
+	// JavaScript ships with some predefined function.
+
+	console.log('decodeURI()');
+	console.log('decodeURIComponent()');
+	console.log('encodeURI()');
+	console.log('encodeURIComponent()');
+	console.log('eval()');
+	console.log('isFinite()');
+	console.log('isNan()');
+	console.log('parseFloat()');
+	console.log('parseInt()');
