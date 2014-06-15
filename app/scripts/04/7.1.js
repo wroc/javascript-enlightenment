53e6ea04ea3552ac2d65fdefda69786b800ab918
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-15 17:38:15 +0200

    Chapter 4: Demo 7.1, The arguments.callee Property

--- app/scripts/04/7.1.js
+++ app/scripts/04/7.1.js
@@ -3,6 +3,6 @@
 	console.log('Chapter 4: Demo 7.1, The arguments.callee Property');
 
 	var foo = function foo() {
-		console.log(arguments.callee);
+		console.log(arguments.callee); //logs foo
+		/* callee could be used to invoke recursively the foo function e.g. arguments.callee()*/
 	};
-
