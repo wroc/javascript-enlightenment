e0a969212b54467223abfb76adce06081cea1c03
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-15 21:25:08 +0200

    Chapter 4: Demo 17.1, Invoking Function Statements before they are defined(Function Hoisting

---
+++ app/scripts/04/18.1.js
@@ -0,0 +1,16 @@
+	'use strict';
+
+	console.log('Chapter 4: Demo 17.1, Invoking Function Statements before they are defined(Function Hoisting');
+
+	//  A function statement can be invoked during execution before its actual definition. 
+
+	// example 1
+	var speak = function() {
+		sayYo();
+		function sayYo() { console.log('Yo');}
+	}();
+
+	// example 2
+	console.log(sum(2, 2)); // invoke sum(), which is not defined yet. but can still be invoked.
+
+	function sum(x, y) { return x + y; }
