ffb951e0a6cf216079f576ee232d650b081df8d0
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-15 23:02:51 +0200

    Chapter 5: Demo 1.1, The Head Global Object

--- app/scripts/05/1.1.js
+++ app/scripts/05/1.1.js
@@ -2,3 +2,12 @@
 
 	console.log('Chapter 5: Demo 1.1, The Head Global Object');
 
+	var myStringVar = 'myString';
+	var myFunctionVar = function() {};
+	myString = 'myString';
+	myFunction = function() {};
+
+	console.log('myStringVar' in window);
+	console.log('myFunctionVar' in window);
+	console.log('myString' in window);
+	console.log('myFunction' in window);
---
+++ app/scripts/05/2.1.js
@@ -0,0 +1,3 @@
+	'use strict';
+
+	console.log('Chapter 5: Demo 1.1, Global Functions Contained Within the Head Object');
