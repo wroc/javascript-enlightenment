7d4d759d6786f4fad1b8a79aee2306f84f806a5f
Manuel Ferreira <mannuel.ferreira@gmail.com>
2014-06-17 05:15:27 +0200

    Chapter 5: Demo 4.1, Refering to the Head Object

---
+++ app/scripts/05/4.1.js
@@ -0,0 +1,12 @@
+	'use strict';
+
+	console.log('Chapter 5: Demo 4.2, Refering to the Head Object');
+
+	var foo =  'bar';
+
+	windowRef1 = window;
+	windowRef2 = this;
+
+	console.log(windowRef1, windowRef2);
+
+	console.log(windowRef1.foo, windowRef2.foo); 
