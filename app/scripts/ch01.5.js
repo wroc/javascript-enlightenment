3338aac80ce4629b046387d83b593e9f88b599ea
wroc <ruby3@eastmt.co.jp>
2015-03-20 17:53:07 +0900

    Update ch01.5.js
    format of source code

--- app/scripts/ch01.5.js
+++ app/scripts/ch01.5.js
@@ -1,31 +1,31 @@
-	'use strict';
+'use strict';
 
-	console.log('Chapter 1: Demo 1.5, Primitive (a.k.a Simple) Values');
+console.log('Chapter 1: Demo 1.5, Primitive (a.k.a Simple) Values');
 
-	var myString = 'string';
-	var myNumber = 10;
-	var myBoolean = false;
-	var myNull = null;
-	var myUndefind = undefined;
+var myString = 'string';
+var myNumber = 10;
+var myBoolean = false;
+var myNull = null;
+var myUndefind = undefined;
 
-	console.log(myString, myNumber, myBoolean, myNull, myUndefind);
-
-
-	/*
-	Consider that a complex object like array or object can be made up of multiple primitive values, and thus becomes a complex set of  multiple values
-	*/
-
-	var myObject = {
-		myString: 'string',
-		myNumber: 10,
-		myBoolean: true,
-		myNull: null,
-		myUndefind: undefined
-	};
-
-	console.log(myObject);
+console.log(myString, myNumber, myBoolean, myNull, myUndefind);
 
 
-	var myArray = ['string', 10, false, null, undefined];
+/*
+Consider that a complex object like array or object can be made up of multiple primitive values, and thus becomes a complex set of  multiple values
+*/
 
-	console.log(myArray);
+var myObject = {
+    myString: 'string',
+    myNumber: 10,
+    myBoolean: true,
+    myNull: null,
+    myUndefind: undefined
+};
+
+console.log(myObject);
+
+
+var myArray = ['string', 10, false, null, undefined];
+
+console.log(myArray);
